ca8542b900e799f82af2fd0bab3a13db42b238da
Joan Lyness <joanrigdon@gmail.com>
2019-02-23 01:50:18 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each(array)
+  i=0
+  array.each_with_index do |element, index|
+  yield
   # code here
+end
+
+yielding do
+  puts "the method has yielded to the block!"
 end
